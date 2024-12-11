f8410a4af4d5aab19d660ebeb4e38dfce44944e4
phamthihaiyen <yen.pth1507@gmail.com>
2024-12-12 00:42:33 +0700

    update Api client get repo form github

--- lib/layers/data/source/api_client.dart
+++ lib/layers/data/source/api_client.dart
@@ -91,4 +91,7 @@ abstract class ApiClient {
 
   @POST('/auth/mobile/github')
   Future<dynamic> loginWithGithub(@Body() Map<String, dynamic> body);
+
+  @GET('/thirdParty/github/repo')
+  Future<BaseResponse> getGithubRepos();
 }
