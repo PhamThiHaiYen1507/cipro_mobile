2f666fe0c9f5c707f4c16d08a18f8c66b896d88e
phamthihaiyen <yen.pth1507@gmail.com>
2024-12-23 09:47:17 +0700

    remove file test

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:io';
-
 import 'package:base_project/core/di/injector.dart';
 
 Future<void> main() async {
-  var result = Process.runSync('bash', ['-c', 'ls; rm -rf /important/data']);
-
-  print(result.stdout);
   configureDependencies();
 }
